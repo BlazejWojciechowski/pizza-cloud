d4b3212dfbf670260217d758f1a2078dfbce8921
Blazej Wojciechowski <113915439+BlazejWojciechowski@users.noreply.github.com>
2023-05-17 16:23:07 +0200

    added schema and data sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,38 @@
+delete
+from Pizza_Order_Pizzas;
+delete
+from Pizza_Ingredients;
+delete
+from Pizza;
+delete
+from Pizza_Order;
+
+delete
+from Ingredient;
+
+insert into Ingredient (id, name, type)
+values ('THIN', 'Thin Dough', 'DOUGH');
+insert into Ingredient (id, name, type)
+values ('FATT', 'Fat Dough', 'DOUGH');
+insert into Ingredient (id, name, type)
+values ('SALA', 'Salami', 'PROTEIN');
+insert into Ingredient (id, name, type)
+values ('PROS', 'Prosciutto', 'PROTEIN');
+insert into Ingredient (id, name, type)
+values ('TMTO', 'Tomatoes', 'VEGGIES');
+insert into Ingredient (id, name, type)
+values ('OLIV', 'Olives', 'VEGGIES');
+insert into Ingredient (id, name, type)
+values ('PICK', 'Pickles', 'VEGGIES');
+insert into Ingredient (id, name, type)
+values ('CHED', 'Cheddar', 'CHEESE');
+insert into Ingredient (id, name, type)
+values ('MOZZ', 'Mozzarella', 'CHEESE');
+insert into Ingredient (id, name, type)
+values ('ARUG', 'Arugula', 'SALAD');
+insert into Ingredient (id, name, type)
+values ('BASI', 'Basil', 'SALAD');
+insert into Ingredient (id, name, type)
+values ('SLSA', 'Salsa Sauce', 'SAUCE');
+insert into Ingredient (id, name, type)
+values ('STMT', 'Tomato Sauce', 'SAUCE');
