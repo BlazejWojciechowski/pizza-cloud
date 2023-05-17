79e3448d0277d4eb03f13bb9e720b301f0730a48
Blazej Wojciechowski <113915439+BlazejWojciechowski@users.noreply.github.com>
2023-05-17 13:21:26 +0200

    Created JDBC repository for Ingredients

---
+++ src/main/java/blaze/springframework/pizza/repositories/IngredientRepository.java
@@ -0,0 +1,9 @@
+package blaze.springframework.pizza.repositories;
+
+import blaze.springframework.pizza.domain.Ingredient;
+
+public interface IngredientRepository {
+    Iterable<Ingredient> findAll();
+    Ingredient findById(String id);
+    Ingredient save(Ingredient ingredient);
+}
