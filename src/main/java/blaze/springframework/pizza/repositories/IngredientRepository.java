0d4f71a3020ac980c7b0e1409472b583e6535c05
Blazej Wojciechowski <113915439+BlazejWojciechowski@users.noreply.github.com>
2023-05-19 13:12:53 +0200

    make some changes

--- src/main/java/blaze/springframework/pizza/repositories/IngredientRepository.java
+++ src/main/java/blaze/springframework/pizza/repositories/IngredientRepository.java
@@ -3,19 +3,7 @@ package blaze.springframework.pizza.repositories;
 import blaze.springframework.pizza.domain.Ingredient;
 
 public interface IngredientRepository {
-<<<<<<< HEAD
-
-    Iterable<Ingredient> findAll();
-
-    Ingredient findById(String id);
-
-    Ingredient save(Ingredient ingredient);
-
-}
-
-=======
     Iterable<Ingredient> findAll();
     Ingredient findById(String id);
     Ingredient save(Ingredient ingredient);
 }
->>>>>>> origin/jdbc
